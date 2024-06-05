2ebeb2cfee34264a1bd994276353e947085c1684
adityaluthra0987 <adityaluthra0987@gmail.com>
2024-06-05 16:38:50 +0530

    Signin/up

--- login/sign up.kt
+++ login/sign up.kt
@@ -1,7 +1,19 @@
-import android.view.View
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_welcome.*
 
-class MyButtonListner : View.OnClockListner {
-    override fun onClick(v: View) {
-        //Code to run when the button is clicked
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        // Create an instance of your listener class
+        val myButtonListener = MyButtonListener()
+
+        // Set the listener for the login_button
+        login_button.setOnClickListener(myButtonListener)
+
+        // Set the listener for the signup_button
+        signup_button.setOnClickListener(myButtonListener)
     }
 }
